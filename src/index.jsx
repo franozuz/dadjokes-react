535df48cc41f302ee39ca8799c3a1eabbb1f7a03
Zuzana Franova <franova.z@gmail.com>
2021-05-29 10:13:07 +0200

    15. ukol

--- src/index.jsx
+++ src/index.jsx
@@ -1,50 +1,19 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
-import './style.css';
+import { Joke } from './Joke';
+import { jokes } from './jokes';
 
 const App = () => {
-  const [likes, setLikes] = useState(0);
-  const [dislikes, setDislikes] = useState(0);
-
-  return (
-    <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={() => setLikes(likes + 1)}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {likes}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={() => setDislikes(dislikes + 1)}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {dislikes}
-          </span>
-        </div>
-      </div>
-    </div>
-  );
+  return jokes.map((joke) => (
+    <Joke
+      key={joke.id}
+      userAvatar={joke.avatar}
+      userName={joke.name}
+      text={joke.text}
+      likes={joke.likes}
+      dislikes={joke.dislikes}
+    />
+  ));
 };
 
 render(<App />, document.querySelector('#app'));
